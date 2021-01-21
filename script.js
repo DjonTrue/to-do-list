e630bf2f0bbfa319d3225a1cde2fede43f6f9a1e
Evgenii Latyshev <djonsons1998@gmail.com>
2021-01-21 18:04:56 +0400

    BUG: second element of array don't arise in DOM

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-console.log("test");
+const inputText = document.getElementById('input-text');
+const createElement = document.getElementById('input-submit');
+const createCasesContainer = document.createElement('div');
+const createCases = createCasesContainer.appendChild(document.createElement('p'));
+const wrapper = document.querySelector('.wrapper');
+const toDoList = [];
 
-function one() {
-    console.log();
+createElement.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (inputText.value === '') return;
+    
+    const newToDo = {
+        name: inputText.value,
+        checked: false,
+        createDate: new Date()
+    };
+    toDoList.push(newToDo);
+    displayElements();
+    console.log(toDoList);
+});
+
+function displayElements() {
+    for (let i = 0; i <= toDoList.length; i++) {
+        wrapper.append(createCasesContainer); // create <div class="cases-container"></div>
+        createCasesContainer.className = 'cases-container';
+        createCasesContainer.id = 'container_' + i;
+        createCases.className = 'cases-text'; // create <p class="'cases-text'"></p>
+        createCases.id = 'text_' + i;
+        createCases.innerHTML = inputText.value;
+    
+        
+    };
 }
 
 // вычеркивание будет реализовано при нажатии на конкретную тудушку
